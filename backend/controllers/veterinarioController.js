36cfb84479cc3f5a14253deea71c337da29e72da
Franco <seranojuliofranciscojavier@gmail.com>
2024-02-25 08:52:25 -0500

    terminamos de hacer las rutas post, get de olvide-password para cambiar la contraseña del veterinario.

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,5 +1,6 @@
 import Veterinario from "../models/Veterinario.js";
 import generarJWT from "../helpers/generarJWT.js";
+import generarId from "../helpers/generarId.js";
 //Para registrar al usuario
 const registrar = async (req, res) => {
   //Aplicar destructuring para extraer los datos del formulario
@@ -73,6 +74,63 @@ const autenticar = async (req,res) => {
     res.json({token:generarJWT(usuario.id)});
 }
 
+
+//Para recuperar la contraseña
+const olvidePassword = async (req,res) => {
+  const {email} = req.body;
+  const existeVeterinario = await Veterinario.findOne({email});
+  if(!existeVeterinario){
+    const error = new Error("Usuario no encontrado");
+    return res.status(400).json({msg:error.message});
+  }
+
+  try{
+    existeVeterinario.token = generarId();
+    await existeVeterinario.save();
+    //Enviar un email al usuario con el token
+    res.json({msg:"Se ha enviado un email con las instrucciones al usuario"})
+  }catch(error){
+    console.log(error);
+  }
+
+}
+
+//Para comprobar el token
+const comprobarToken = async (req,res) => {
+  const {token}  = req.params;
+
+  const tokenValido = await Veterinario.findOne({token});
+  if(!tokenValido){
+    const error = new Error("Token no válido");
+    return res.status(400).json({msg:error.message});
+  }
+
+  res.json({msg:"Token válido y el usuario existe"});
+
+}
+//Para crear un nuevo password
+const nuevoPassword = async (req,res) => {
+  //Lo que enviamos en la url
+  const {token} = req.params;
+  //Lo que el usuario escribas en el formulario
+  const {password} = req.body;
+
+  const veterinario = await Veterinario.findOne({token});
+  if(!veterinario){
+    const error = new Error("Hubo un error");
+    return res.status(400).json({msg:error.message});
+  }
+  try{
+    veterinario.token = null;
+    veterinario.password = password;
+    await veterinario.save();
+    res.json({msg:"Password actualizado correctamente"});
+
+  }catch(error){
+    console.log(error);
+  }
+}
+
 const perfil = (req, res) => {
   const {veterinario } = req;
   res.json({
@@ -80,4 +138,4 @@ const perfil = (req, res) => {
   });
 };
 
-export { registrar, perfil, confirmar, autenticar };
+export { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword };
--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,17 +1,23 @@
 import express from "express";
-import { registrar, perfil, confirmar, autenticar } from "../controllers/veterinarioController.js";
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from "../controllers/veterinarioController.js";
 import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+//Area pública
 //Routing de registrar(para registrar al veterinario , hacemos una peticion get y cargue el formulario de registro del veterinario)
 router.post("/",registrar);
 //Para confirmar bya token al veterinario que se registró
 router.get("/confirmar/:token",confirmar);
 //Para autenticar al veterinario y iniciar sesion
 router.post("/login", autenticar);
+//Param validar el email del usuario
+router.post("/olvide-password", olvidePassword);
+//Para validar el token
+//Para crear otro nuevo password
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
-
+//Area privada
 //Routing de login(para cuando vayamos a iniciar sesion hagamos una peticion get y cargue el perfil del veterinario que se logueo)
 router.get("/perfil", checkAuth,perfil);
 
